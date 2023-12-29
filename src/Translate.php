7743861726ef66eac6c9467d25f5c7c8a854826c
allofmex <allofmex@web.de>
2023-12-29 08:12:15 +0100

    Improved cache/project-root dir handling

--- src/Translate.php
+++ src/Translate.php
@@ -16,15 +16,10 @@ class Translate {
     static $cacheDir = null;
 
     public static function translateFile($fileToTranslate, $locale) {
-        if (self::$cacheDir === null) {
-            self::$cacheDir = $_SERVER['DOCUMENT_ROOT'].'/../var/cache';
-        }
-        if (!file_exists(self::$cacheDir)) {
-            mkdir(self::$cacheDir, 0700, true);
-        }
-        $cacheFile = self::$cacheDir.'/'.$locale.'_'.basename($fileToTranslate);
+        $cacheDir = self::getCacheDir();
+        $cacheFile = $cacheDir.'/'.$locale.'_'.basename($fileToTranslate);
         $langFile = self::getLangFile($locale);
-        $langFilePhp = self::$cacheDir.'/lang-file_'.$locale.'.php';
+        $langFilePhp = $cacheDir.'/lang-file_'.$locale.'.php';
         $mTimeFile = filemtime($fileToTranslate);
         $forceUpdate = false;
 
@@ -42,7 +37,8 @@ class Translate {
     }
 
     private static function getStringsForLocale($locale) : array {
-        $langFilePhp = self::$cacheDir.'/lang-file_'.$locale.'.php';
+        $cacheDir = self::getCacheDir();
+        $langFilePhp = $cacheDir.'/lang-file_'.$locale.'.php';
         if (!key_exists($locale, self::$strings)) {
             self::$strings[$locale] = include $langFilePhp;
         }
@@ -52,7 +48,7 @@ class Translate {
     /**
      * Warning! This is only partially cached and should not be used for production.
      * You may call this in testing.
-     * 
+     *
      * @param string $text plain string to translate (without {t})
      * @param string $locale
      * @return string
@@ -62,7 +58,7 @@ class Translate {
     }
 
     /**
-     * 
+     *
      * @param string $rawText original/untranslated text
      * @param string $langFilePhp
      * @return string translated text
@@ -78,23 +74,23 @@ class Translate {
 
     private static function findTranslateAndRestore(string $orgText, $strings) {
         $key = $orgText;
-            if (strlen($key) > self::MAX_KEY_LENGTH) {
-                $key = substr($key, 0, self::MAX_KEY_LENGTH);
-            }
-            // search for not-to-translate section {n}keep{/n}
-            $ignoreStart = strpos($orgText, '{n}');
-            $hasRestoreSection = $ignoreStart !== false;
-            // key to match entry in translation files is first part until {n} (if existing)
-            // and max length is MAX_KEY_LENGTH
-            $key = trim($hasRestoreSection ? substr($key, 0, $ignoreStart) : $key);
-            $translatedText = isset($strings[$key]) ? $strings[$key] : $orgText;
+        if (strlen($key) > self::MAX_KEY_LENGTH) {
+            $key = substr($key, 0, self::MAX_KEY_LENGTH);
+        }
+        // search for not-to-translate section {n}keep{/n}
+        $ignoreStart = strpos($orgText, '{n}');
+        $hasRestoreSection = $ignoreStart !== false;
+        // key to match entry in translation files is first part until {n} (if existing)
+        // and max length is MAX_KEY_LENGTH
+        $key = trim($hasRestoreSection ? substr($key, 0, $ignoreStart) : $key);
+        $translatedText = isset($strings[$key]) ? $strings[$key] : $orgText;
 
-            if ($hasRestoreSection) {
-                // restore not-to-translate section from original texts section
-                return self::restore($orgText, $translatedText);
-            } else {
-                return $translatedText;
-            }
+        if ($hasRestoreSection) {
+            // restore not-to-translate section from original texts section
+            return self::restore($orgText, $translatedText);
+        } else {
+            return $translatedText;
+        }
     }
 
     private static function restore($originalText, $translatedText) {
@@ -110,10 +106,9 @@ class Translate {
         };
         return preg_replace_callback('/{n}([\s\S]+?){\/n}/', $ignore, $translatedText, -1, $count);
     }
-    
+
     private static function getLangFile($locale) {
-        // return _SERVER['DOCUMENT_ROOT'].'/../translations/'.$locale.'.ini';
-        return $_SERVER['DOCUMENT_ROOT'].'/../translations/'.$locale.'.yml';
+        return self::getProjectRootDir().'/translations/'.$locale.'.yml';
     }
 
     private static function parseLangFile($langFile, $langFilePhp) {
@@ -139,8 +134,27 @@ class Translate {
         } else {
             $rawData = array();
         }
-
         file_put_contents($langFilePhp, '<?php return '.var_export($rawData, true).';', LOCK_EX);
+    }
+
+    private static function getCacheDir() {
+        if (self::$cacheDir === null) {
+            self::$cacheDir = self::getProjectRootDir().'/var/cache';
+            if (!file_exists(self::$cacheDir)) {
+                mkdir(self::$cacheDir, 0700, true);
+            }
+        }
+        return self::$cacheDir;
+    }
+
+    private static function getProjectRootDir() {
+        if (isset($_SERVER['DOCUMENT_ROOT']) && $_SERVER['DOCUMENT_ROOT'] !== '') {
+            $docRoot = $_SERVER['DOCUMENT_ROOT'].'/..';
+        } else {
+            // fallback for testing,...
+            $docRoot = getcwd();
+        }
+        return $docRoot;
     }
 }
 
